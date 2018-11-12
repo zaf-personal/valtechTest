1e6ef9b2a916b12f840b44f899043cefc2588bcc
zfardellas <zfardellas@defactointeractif.com>
2018-11-12 09:21:12 -0500

    Reworked Tabs

---
+++ src/components/GraphTabs.vue
@@ -0,0 +1,272 @@
+<template>
+  <div>
+    <div class="graph-tabs">
+      <ul>
+        <li :class="{ 'active' : activetab === 'lorem' }"><a @click="activetab='lorem'">lorem</a></li>
+        <li :class="{ 'active' : activetab === 'ipsum' }"><a @click="activetab='ipsum'">ipsum</a></li>
+        <li :class="{ 'active' : activetab === 'amet' }"><a @click="activetab='amet'">amet</a></li>
+      </ul>
+    </div>
+    <div class="graphs">
+      <div class=y-legend>$B</div>
+      <div class=x-legend>Year</div>
+      <div v-show="activetab === 'lorem'" class="chart-wrapper">
+        <apexcharts width="250" type="bar" :options="chartOptions1" :series="series1"></apexcharts>
+      </div>
+      <div v-show="activetab === 'ipsum'" class="chart-wrapper">
+        <apexcharts width="250" type="bar" :options="chartOptions2" :series="series2"></apexcharts>
+      </div>
+      <div v-show="activetab === 'amet'" class="chart-wrapper">
+        <apexcharts width="250" type="bar" :options="chartOptions3" :series="series3"></apexcharts>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+export default {
+  name: "GraphTabs",
+  components: {
+    apexcharts: VueApexCharts
+  },
+  data() {
+    return {
+      activetab: "lorem",
+      chartOptions1: {
+        chart: {
+          id: "basic-bar",
+          selection: {
+            enabled: false
+          }
+        },
+        fill: {
+          colors: "#e6ff00",
+          opacity: 1
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: [2018, 2019, 2020, 2021, 2022],
+          labels: {
+            style: {
+              colors: ["#fff", "#fff", "#fff", "#fff", "#fff"]
+            }
+          }
+        },
+        yaxis: {
+          tickAmount: 6,
+          min: 20,
+          max: 80,
+          labels: {
+            style: {
+              color: "#fff"
+            }
+          }
+        },
+        tooltip: {
+          enabled: false
+        },
+        grid: {
+          borderColor: "#fff",
+          padding: {
+            bottom: 1
+          }
+        }
+      },
+      chartOptions2: {
+        chart: {
+          id: "basic-bar",
+          selection: {
+            enabled: false
+          }
+        },
+        fill: {
+          colors: "#61e3e8",
+          opacity: 1
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: [2018, 2019, 2020, 2021, 2022],
+          labels: {
+            style: {
+              colors: ["#fff", "#fff", "#fff", "#fff", "#fff"]
+            }
+          }
+        },
+        yaxis: {
+          tickAmount: 6,
+          min: 20,
+          max: 80,
+          labels: {
+            style: {
+              color: "#fff"
+            }
+          }
+        },
+        tooltip: {
+          enabled: false
+        },
+        grid: {
+          borderColor: "#fff",
+          padding: {
+            bottom: 1
+          }
+        }
+      },
+      chartOptions3: {
+        chart: {
+          id: "basic-bar",
+          selection: {
+            enabled: false
+          }
+        },
+        fill: {
+          colors: "#ffa32b",
+          opacity: 1
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: [2018, 2019, 2020, 2021, 2022],
+          labels: {
+            style: {
+              colors: ["#fff", "#fff", "#fff", "#fff", "#fff"]
+            }
+          }
+        },
+        yaxis: {
+          tickAmount: 6,
+          min: 20,
+          max: 80,
+          labels: {
+            style: {
+              color: "#fff"
+            }
+          }
+        },
+        tooltip: {
+          enabled: false
+        },
+        grid: {
+          borderColor: "#fff",
+          padding: {
+            bottom: 1
+          }
+        }
+      },
+      series1: [
+        {
+          name: "Lorem",
+          data: [40, 50, 55, 60, 75]
+        }
+      ],
+      series2: [
+        {
+          name: "Ipsum",
+          data: [60, 35, 70, 80, 35]
+        }
+      ],
+      series3: [
+        {
+          name: "Amet",
+          data: [55, 40, 35, 30, 25]
+        }
+      ]
+    };
+  },
+  computed: {
+    href() {
+      return "#" + this.name.toLowerCase().replace(/ /g, "-");
+    }
+  },
+  mounted() {
+    this.isActive = this.selected;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.graph-tabs {
+  margin-top: -7px;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-content: flex-start;
+    li {
+      flex: 0 1 51px;
+      margin-right: 10px;
+      background-color: transparent;
+      color: #e6ff00;
+      border: 1px solid #e6ff00;
+      &:hover,
+      &.active {
+        color: #002140;
+        background-color: #e6ff00;
+        border-color: #e6ff00;
+      }
+      &:nth-child(2) {
+        color: #61e3e8;
+        border-color: #61e3e8;
+        &:hover,
+        &.active {
+          color: #002140;
+          background-color: #61e3e8;
+          border-color: #61e3e8;
+        }
+      }
+      &:last-child {
+        color: #ffa32b;
+        border-color: #ffa32b;
+        &:hover,
+        &.active {
+          color: #002140;
+          background-color: #ffa32b;
+          border-color: #ffa32b;
+        }
+      }
+      a {
+        display: block;
+        text-align: center;
+        font-size: 10px;
+        line-height: 20px;
+        color: inherit;
+        text-decoration: none;
+        text-transform: uppercase;
+      }
+    }
+  }
+}
+.graphs {
+  clear: both;
+  position: relative;
+  width: 250px;
+  margin-top: 30px;
+  font-size: 11px;
+  font-weight: bold;
+  color: #fff;
+}
+.y-legend {
+  position: absolute;
+  top: -5px;
+  left: 7px;
+}
+.x-legend {
+  position: absolute;
+  bottom: 12px;
+  right: -25px;
+}
+</style>
+<!-- Not scoped to modify chart style -->
+<style lang="scss">
+.apexcharts-grid {
+  display: none;
+}
+</style>
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,32 +1,168 @@
 <template>
   <div>
-      <div class="tabs">
+      <div class="tabs" v-if="!isMobile">
         <ul>
-          <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }" :key="tab.name">
-            <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
-          </li>
+          <li :class="{ 'active' : activetab === 1998 }"><a @click="activetab=1998">1998</a></li>
+          <li :class="{ 'active' : activetab === 2002 }"><a @click="activetab=2002">2002</a></li>
+          <li :class="{ 'active' : activetab === 2006 }"><a @click="activetab=2006">2006</a></li>
+          <li :class="{ 'active' : activetab === 2012 }"><a @click="activetab=2012">2012</a></li>
+          <li :class="{ 'active' : activetab === 2014 }"><a @click="activetab=2014">2014</a></li>
+          <li :class="{ 'active' : activetab === 2018 }"><a @click="activetab=2018">2018</a></li>
         </ul>
       </div>
       <div class="tabs-details">
-        <slot></slot>
+        <div class="accordion-tab" :class="{ 'active' : activetab === 1998 }" v-if="isMobile">
+          <a @click="activetab=1998">1998</a>
+        </div>
+        <div v-show="activetab === 1998" class="tab-content-wrapper">
+          <h3>Lorem ipsum projection lorem</h3>
+          <div class="tab-panel">
+            <div class="tab-panel-content">
+              <p>1998 ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Tableau des lorem ipsum</p>
+              <GraphTabs />
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-tab" v-if="isMobile" :class="{ 'active' : activetab === 2002 }">
+          <a @click="activetab=2002">2002</a>
+        </div>
+        <div v-show="activetab === 2002" class="tab-content-wrapper">
+          <h3>Lorem ipsum projection lorem</h3>
+          <div class="tab-panel">
+            <div class="tab-panel-content">
+              <p>2002 ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Tableau des lorem ipsum</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-tab" v-if="isMobile" :class="{ 'active' : activetab === 2006 }">
+          <a @click="activetab=2006">2006</a>
+        </div>
+        <div v-show="activetab === 2006" class="tab-content-wrapper">
+          <h3>Lorem ipsum projection lorem</h3>
+          <div class="tab-panel">
+            <div class="tab-panel-content">
+              <p>2006 ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Tableau des lorem ipsum</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-tab" v-if="isMobile" :class="{ 'active' : activetab === 2012 }">
+          <a @click="activetab=2012">2012</a>
+        </div>
+        <div v-show="activetab === 2012" class="tab-content-wrapper">
+          <h3>Lorem ipsum projection lorem</h3>
+          <div class="tab-panel">
+            <div class="tab-panel-content">
+              <p>2012 ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Tableau des lorem ipsum</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-tab" v-if="isMobile" :class="{ 'active' : activetab === 2014 }">
+          <a @click="activetab=2014">2014</a>
+        </div>
+        <div v-show="activetab === 2014" class="tab-content-wrapper">
+          <h3>Lorem ipsum projection lorem</h3>
+          <div class="tab-panel">
+            <div class="tab-panel-content">
+              <p>2014 ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Tableau des lorem ipsum</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-tab" v-if="isMobile" :class="{ 'active' : activetab === 2018 }">
+          <a @click="activetab=2018">2018</a>
+        </div>
+        <div v-show="activetab === 2018" class="tab-content-wrapper">
+          <h3>Lorem ipsum projection lorem</h3>
+          <div class="tab-panel">
+            <div class="tab-panel-content">
+              <p>2018 ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+              <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+            </div>
+            <div class="tab-panel-content">
+              <p>Tableau des lorem ipsum</p>
+            </div>
+          </div>
+        </div>
       </div>
   </div>
 </template>
 
 <script>
+import GraphTabs from "@/components/GraphTabs.vue";
 export default {
   name: "Tabs",
-  data() {
-    return { tabs: [] };
+  components: {
+    GraphTabs
   },
-  created() {
-    this.tabs = this.$children;
+  data() {
+    return {
+      activetab: 1998,
+      isMobile: false
+    };
+  },
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener("resize", this.getWindowWidth);
+      //Init
+      this.getWindowWidth();
+    });
   },
   methods: {
-    selectTab(selectedTab) {
-      this.tabs.forEach(tab => {
-        tab.isActive = tab.name == selectedTab.name;
-      });
+    getWindowWidth() {
+      this.windowWidth = document.documentElement.clientWidth;
+      if (this.windowWidth < 768) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
     }
   }
 };
@@ -34,4 +170,147 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.timeline-success {
+  padding: 48px 40px;
+  background-color: #002140;
+}
+h2 {
+  font-size: 32px;
+  color: #fff;
+  font-weight: normal;
+  margin-bottom: 23px;
+}
+h3 {
+  font-size: 24px;
+  font-weight: normal;
+  color: #e6ff00;
+  width: 100%;
+  margin-bottom: 20px;
+}
+//Tab Nav Item Style
+.tabs {
+  width: 100.1%;
+  border-bottom: 1px solid #fff;
+  margin-bottom: 59px;
+  > ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-content: flex-start;
+    position: relative;
+    top: 15px;
+    z-index: 10;
+    > li {
+      flex: 0 1 54px;
+      color: #fff;
+      background-color: #002140;
+      border: 1px solid #fff;
+      &:hover,
+      &.active {
+        color: #002140;
+        background-color: #e6ff00;
+        border-color: #e6ff00;
+      }
+      > a {
+        display: block;
+        text-align: center;
+        font-size: 14px;
+        line-height: 30px;
+        color: inherit;
+        text-decoration: none;
+      }
+    }
+  }
+}
+//Accordion Tab Style
+.accordion-tab {
+  display: block;
+  width: 54px;
+  &:first-child {
+    margin-top: 39px;
+  }
+  &:after {
+    content: "|";
+    display: block;
+    color: #fff;
+    text-align: center;
+    font-size: 12px;
+  }
+  &:nth-child(11) {
+    &:after {
+      content: " ";
+    }
+  }
+  &.active {
+    &:after {
+      content: "";
+      margin-bottom: 15px;
+    }
+  }
+  a {
+    color: #fff;
+    background-color: #002140;
+    border: 1px solid #fff;
+    display: block;
+    text-align: center;
+    font-size: 14px;
+    line-height: 30px;
+    text-decoration: none;
+  }
+  &:hover a,
+  &.active a {
+    color: #002140;
+    background-color: #e6ff00;
+    border-color: #e6ff00;
+  }
+}
+//Tab Content Styles
+.tab-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  .tab-panel-content {
+    flex: 0 1 auto;
+    p {
+      margin-bottom: 17px;
+      font-size: 16px;
+      font-weight: normal;
+      line-height: 26px;
+      color: #f4e8d6;
+    }
+  }
+}
+//Animate Tab Content
+.tabs-details > div {
+  animation: panelFadeIn 4s ease;
+}
+@keyframes panelFadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@media (min-width: 768px) {
+  h2 {
+    font-size: 36px;
+  }
+  h3 {
+    width: 30%;
+    margin-bottom: 30px;
+  }
+  .tab-panel {
+    flex-direction: row;
+    .tab-panel-content {
+      flex: 0 1 30%;
+    }
+  }
+  .accordion-tab {
+    &:after {
+      content: "";
+    }
+  }
+}
 </style>
